2ab3757907dbe00872bb60636407beb733a5b7e6
zwjin <qq710327563@126.com>
2023-08-03 10:25:59 +0800

    增加OK咖啵规则

--- js/capooGameInit.js
+++ js/capooGameInit.js
@@ -1,8 +1,10 @@
 var locations = [];
-var clickTimes = 7;
+var visitedLocation = [];
 var model = {
     capooGameSize: 7,
     okCapooNum: 3,
+    remainTimes: 20,
+    hitTimes: 0,
 
     generateCapooLocations: function() {
         for (let i = 0; i < this.okCapooNum; i++) {
@@ -18,11 +20,26 @@ var model = {
 }
 
 
+function displayTimes() {
+    let times = document.getElementById("times");
+    times.innerHTML = "remain: " + model.remainTimes;
+}
+
+function checkWin() {
+    let times = document.getElementById("times");
+    if (model.hitTimes >= model.okCapooNum) {
+        times.innerHTML = "WIN!";
+    }
+    if (model.remainTimes <= 0 && model.hitTimes < model.okCapooNum) {
+        times.innerHTML = "GAME OVER!";
+    }
+}
+
 
 function init() {
-    let table = document.getElementById("gameTable");
+    let gameTable = document.getElementById("gameTable");
     for (let i = 0; i < 7; i++) {
-        let newTr = table.insertRow(i);
+        let newTr = gameTable.insertRow(i);
         for (let j = 0; j < 7; j++) {
             let newTd = newTr.insertCell(j);
             let location = String(i) + String(j);
@@ -31,24 +48,36 @@ function init() {
             newTd.addEventListener("click", 
             function() {
                 hit(location);
+                displayTimes();
+                checkWin();
             }, 
             false);
-            
         }
     }
-
+    let ruleTable = document.getElementById("ruleTable");
+    let ruleTr = ruleTable.insertRow(0);
+    let textTd = ruleTr.insertCell(0);
+    textTd.setAttribute("id", "times");
+    textTd.innerHTML = "remain: " + model.remainTimes;
     model.generateCapooLocations();
 }
 
 function hit(location) {
+    if (visitedLocation.indexOf(location) >= 0) {
+        return;
+    }
+    model.remainTimes--;
+    console.log(model.remainTimes);
     let img = document.getElementById("img_" + location);
     for (let i = 0; i < locations.length; i++) {
         if (locations[i] == location) {
             img.setAttribute("src", "img/ok.gif");
+            model.hitTimes++;
             return;
         }
     }
     img.setAttribute("src", "img/no.gif");
+    visitedLocation.push(location);
 }
 
 window.onload = init;
